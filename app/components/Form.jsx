cc2797e9a8f596eea40bb8e88f03e434f21f799a
lloydeebill <lloydbill.lariosa@gmail.com>
2025-04-12 05:00:17 -0400

    fixed useEffect problem

--- app/components/Form.jsx
+++ app/components/Form.jsx
@@ -1,5 +1,7 @@
+"use client";
+
+import { useState, useEffect } from "react";
 import { supabase } from "@/lib/supabaseClient";
-import { useState } from "react";
 import { uploadFile } from "@/utils/uploadFile";
 import RegistrantFields from "./RegistrantFields";
 
